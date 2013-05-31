2ee4dc97681b65fe1a15c662543f6fdfb3e7aaf1
() <nilclass@riseup.net>
2013-05-31 09:33:10 +0200

    auth.c: handle redirect_uri as last parameter correctly

--- src/auth.c
+++ src/auth.c
@@ -34,18 +34,24 @@ void auth_get(struct evhttp_request *request) {
     // FIXME: cleanup this code, it's unreadable.
     const char *redirect_uri_start = redirect_uri_param + 13;
     const char *next_param = strstr(redirect_uri_start, "&");
-    size_t redirect_uri_len = next_param - redirect_uri_start;
+    size_t redirect_uri_len = next_param ? next_param - redirect_uri_start : strlen(redirect_uri_start);
     char encoded_redirect_uri[redirect_uri_len + 1];
     strncpy(encoded_redirect_uri, redirect_uri_start, redirect_uri_len);
     size_t result_len = 0;
     encoded_redirect_uri[redirect_uri_len] = 0; // why doesn't this happen automatically?
     char *clean_redirect_uri = evhttp_uridecode(encoded_redirect_uri, 0, &result_len);
     clean_redirect_uri = realloc(clean_redirect_uri, result_len + strlen(RS_TOKEN) + 14 + 1);
+    if(clean_redirect_uri == NULL) {
+      perror("realloc() failed");
+      evhttp_send_error(request, HTTP_INTERNAL, NULL);
+      return;
+    }
     strcat(clean_redirect_uri, "#access_token=");
     strcat(clean_redirect_uri, RS_TOKEN);
     struct evkeyvalq *headers = evhttp_request_get_output_headers(request);
     evhttp_add_header(headers, "Location", clean_redirect_uri);
     evhttp_send_reply(request, HTTP_MOVETEMP, NULL, NULL);
+    free(clean_redirect_uri);
   } else {
     evhttp_send_error(request, HTTP_BADREQUEST, NULL);
   }
