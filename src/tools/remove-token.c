1085ec8465f1b5272a8ffd55e6c620772c41289d
() <nilclass@riseup.net>
2013-06-16 22:57:26 +0200

    added missing license headers

--- src/tools/remove-token.c
+++ src/tools/remove-token.c
@@ -1,3 +1,14 @@
+/*
+ * rs-serve - (c) 2013 Niklas E. Cathor
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #include <stdio.h>
 #include <stdlib.h>
