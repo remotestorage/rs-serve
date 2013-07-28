2ccbcf3909642fd6c1ca4e477ab72a274d7c671b
Niklas Cathor <nilclass@riseup.net>
2013-07-28 22:42:00 +0200

    auth: fixed remove_authorization

--- src/common/auth.c
+++ src/common/auth.c
@@ -65,6 +65,30 @@ void close_authorizations() {
   auth_db = NULL;
 }
 
+int remove_authorization(struct rs_authorization *auth) {
+  uint32_t keylen = strlen(auth->username) + strlen(auth->token) + 1;
+  char *key = malloc(keylen + 1);
+  if(key == NULL) {
+    perror("Failed to allocate memory");
+    return NULL;
+  }
+  sprintf(key, "%s|%s", auth->username, auth->token);
+  DBT db_key, db_value;
+  memset(&db_key, 0, sizeof(db_key));
+  memset(&db_value, 0, sizeof(db_value));
+  db_key.data = key;
+  db_key.size = keylen;
+  db_key.ulen = keylen + 1;
+  int result = auth_db->del(auth_db, NULL, &db_key, 0);
+  if(result != 0) {
+    if(result != DB_NOTFOUND) {
+      fprintf(stderr, "auth_db->del() failed\n");
+    }
+    return result;
+  }
+  return 0;
+}
+
 
 struct rs_authorization *lookup_authorization(const char *username, const char *token) {
   uint32_t keylen = strlen(username) + strlen(token) + 1;
@@ -285,7 +309,7 @@ void list_authorizations() {
   if(get_result != 0) {
     char *msg;
     switch(get_result) {
-    case DB_NOTFOUND: msg = "DB_NOTFOUND"; break;
+    case DB_NOTFOUND: return;
     case DB_BUFFER_SMALL: msg = "DB_BUFFER_SMALL"; break;
     case DB_LOCK_DEADLOCK: msg = "DB_LOCK_DEADLOCK"; break;
     case DB_LOCK_NOTGRANTED: msg = "DB_LOCK_NOTGRANTED"; break;
@@ -327,16 +351,16 @@ void list_authorizations() {
 
 // below here naive, flat-file based implementation.
 
-FILE *auth_fp = NULL;
+/* FILE *auth_fp = NULL; */
 
-static void free_scopes(struct rs_scope *scope) {
-  /* struct rs_scope *tmp; */
-  /* while(scope) { */
-  /*   tmp = scope; */
-  /*   scope = scope->next; */
-  /*   free(tmp); */
-  /* } */
-}
+/* static void free_scopes(struct rs_scope *scope) { */
+/*   /\* struct rs_scope *tmp; *\/ */
+/*   /\* while(scope) { *\/ */
+/*   /\*   tmp = scope; *\/ */
+/*   /\*   scope = scope->next; *\/ */
+/*   /\*   free(tmp); *\/ */
+/*   /\* } *\/ */
+/* } */
 
 /* void open_authorizations(char *mode) { */
 /*   auth_fp = fopen(RS_AUTH_FILE_PATH, mode); */
@@ -366,54 +390,54 @@ static void free_scopes(struct rs_scope *scope) {
 /*   return 0; */
 /* } */
 
-struct rs_authorization *parse_authorization(struct rs_authorization *auth, char *buf) {
-  /* char *saveptr; */
-  /* auth->username = strtok_r(buf, "|", &saveptr); */
-  /* auth->token = strtok_r(NULL, "|", &saveptr); */
-  /* auth->scopes = NULL; */
-  /* struct rs_scope *scope = NULL; */
-  /* for(;;) { */
-  /*   char *scope_name; */
-  /*   if(*saveptr == ':') { */
-  /*     scope_name = ""; */
-  /*     saveptr++; */
-  /*   } else { */
-  /*     scope_name = strtok_r(NULL, ":", &saveptr); */
-  /*   } */
-  /*   if(scope_name) { */
-  /*     scope = malloc(sizeof(struct rs_scope)); */
-  /*     if(scope == NULL) { */
-  /*       perror("malloc() failed"); */
-  /*       free_scopes(auth->scopes); */
-  /*       return NULL; */
-  /*     } */
-  /*     scope->name = scope_name; */
-  /*     scope->write = (*saveptr == 'w' ? 1 : 0); */
-  /*     while(*saveptr && *saveptr != ',') saveptr++; */
-  /*     if(*saveptr) saveptr++; */
-  /*     scope->next = auth->scopes; */
-  /*     auth->scopes = scope; */
-  /*   } else { */
-  /*     return auth; */
-  /*   } */
-  /* } */
-}
+/* struct rs_authorization *parse_authorization(struct rs_authorization *auth, char *buf) { */
+/*   /\* char *saveptr; *\/ */
+/*   /\* auth->username = strtok_r(buf, "|", &saveptr); *\/ */
+/*   /\* auth->token = strtok_r(NULL, "|", &saveptr); *\/ */
+/*   /\* auth->scopes = NULL; *\/ */
+/*   /\* struct rs_scope *scope = NULL; *\/ */
+/*   /\* for(;;) { *\/ */
+/*   /\*   char *scope_name; *\/ */
+/*   /\*   if(*saveptr == ':') { *\/ */
+/*   /\*     scope_name = ""; *\/ */
+/*   /\*     saveptr++; *\/ */
+/*   /\*   } else { *\/ */
+/*   /\*     scope_name = strtok_r(NULL, ":", &saveptr); *\/ */
+/*   /\*   } *\/ */
+/*   /\*   if(scope_name) { *\/ */
+/*   /\*     scope = malloc(sizeof(struct rs_scope)); *\/ */
+/*   /\*     if(scope == NULL) { *\/ */
+/*   /\*       perror("malloc() failed"); *\/ */
+/*   /\*       free_scopes(auth->scopes); *\/ */
+/*   /\*       return NULL; *\/ */
+/*   /\*     } *\/ */
+/*   /\*     scope->name = scope_name; *\/ */
+/*   /\*     scope->write = (*saveptr == 'w' ? 1 : 0); *\/ */
+/*   /\*     while(*saveptr && *saveptr != ',') saveptr++; *\/ */
+/*   /\*     if(*saveptr) saveptr++; *\/ */
+/*   /\*     scope->next = auth->scopes; *\/ */
+/*   /\*     auth->scopes = scope; *\/ */
+/*   /\*   } else { *\/ */
+/*   /\*     return auth; *\/ */
+/*   /\*   } *\/ */
+/*   /\* } *\/ */
+/* } */
 
-struct rs_authorization *read_authorization(struct rs_authorization *auth, char **buf, size_t *buflen) {
-  size_t linelen;
-  for(;;) {
-    if((linelen = getline(buf, buflen, auth_fp)) == -1) {
-      if(! feof(auth_fp)) {
-        perror("getline() failed");
-      }
-      return NULL;
-    }
-    if(**buf != '\n') { // empty line
-      break;
-    }
-  }
-  return parse_authorization(auth, *buf);
-}
+/* struct rs_authorization *read_authorization(struct rs_authorization *auth, char **buf, size_t *buflen) { */
+/*   size_t linelen; */
+/*   for(;;) { */
+/*     if((linelen = getline(buf, buflen, auth_fp)) == -1) { */
+/*       if(! feof(auth_fp)) { */
+/*         perror("getline() failed"); */
+/*       } */
+/*       return NULL; */
+/*     } */
+/*     if(**buf != '\n') { // empty line */
+/*       break; */
+/*     } */
+/*   } */
+/*   return parse_authorization(auth, *buf); */
+/* } */
 
 /* void list_authorizations() { */
 /*   struct rs_authorization auth; */
@@ -428,27 +452,27 @@ struct rs_authorization *read_authorization(struct rs_authorization *auth, char 
 /*   } */
 /* } */
 
-int remove_authorization(struct rs_authorization *auth) {
-  int uname_len = strlen(auth->username);
-  int token_len = strlen(auth->token);
-  char *buf = NULL;
-  size_t buflen = 0, linelen;
-  while((linelen = getline(&buf, &buflen, auth_fp)) != -1) {
-    if(strncmp(buf, auth->username, uname_len) == 0 &&
-       buf[uname_len] == '|' &&
-       strncmp(buf + uname_len + 1, auth->token, token_len) == 0 &&
-       buf[uname_len + 1 + token_len] == '|') {
-      fseek(auth_fp, ftell(auth_fp) - linelen, SEEK_SET);
-      int j;
-      for(j=0;j<linelen;j++) {
-        fwrite("\n", 1, 1, auth_fp);
-      }
-      fflush(auth_fp);
-      return 0;
-    }
-  }
-  return -1;
-}
+/* int remove_authorization(struct rs_authorization *auth) { */
+/*   int uname_len = strlen(auth->username); */
+/*   int token_len = strlen(auth->token); */
+/*   char *buf = NULL; */
+/*   size_t buflen = 0, linelen; */
+/*   while((linelen = getline(&buf, &buflen, auth_fp)) != -1) { */
+/*     if(strncmp(buf, auth->username, uname_len) == 0 && */
+/*        buf[uname_len] == '|' && */
+/*        strncmp(buf + uname_len + 1, auth->token, token_len) == 0 && */
+/*        buf[uname_len + 1 + token_len] == '|') { */
+/*       fseek(auth_fp, ftell(auth_fp) - linelen, SEEK_SET); */
+/*       int j; */
+/*       for(j=0;j<linelen;j++) { */
+/*         fwrite("\n", 1, 1, auth_fp); */
+/*       } */
+/*       fflush(auth_fp); */
+/*       return 0; */
+/*     } */
+/*   } */
+/*   return -1; */
+/* } */
 
 /* struct rs_authorization *lookup_authorization(const char *username, const char *token) { */
 /*   int uname_len = strlen(username); */
